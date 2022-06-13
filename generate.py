545d5f44e232cbbcd03bef2878833f1456059ac0
fujinov <98008144+fujinov@users.noreply.github.com>
2022-06-13 22:38:07 +0900

    スクリプトとしての処理を追加

--- generate.py
+++ generate.py
@@ -3,6 +3,7 @@ import random
 
 class GenerateMaze(): 
 
+    # widthとheightは共に奇数かつ5以上の整数
     def __init__(self, width, height):
         self.width = width
         self.height = height
@@ -53,7 +54,14 @@ class GenerateMaze():
                 y += i
             stack.append((y, x))
 
-    def print_maze(self):
+    def print_maze(self, print_width=2):
+        for line in self.maze:
+            for char in line:
+                print(char.center(print_width), end='')
+            print()
+
+    # 全角で出力
+    def _print_maze_large(self):
         for lines in self.maze:
             for line in lines:
                 if line == '#':
@@ -66,12 +74,43 @@ class GenerateMaze():
                     print('Ｇ', end='')
             print()
 
-    def preset_start_goal(self):
-        self.maze[self.height-2][0] = 'S'
-        self.maze[1][self.width-1] = 'G'
+    # startとgoalはそれぞれタプルで(y,x)の座標
+    def preset_start_goal(self, start=None, goal=None):
+        if start is None:
+            self.start = (self.height-2, 0)
+            self.maze[self.height-2][0] = 'S'
+        else:
+            self.start = start
+            self.maze[start[0]][start[1]] = 'S'
+
+        if goal is None:
+            self.goal = (1, self.width-1)
+            self.maze[1][self.width-1] = 'G'
+        else:
+            self.goal = goal
+            self.maze[goal[0]][goal[1]] = 'G'
+
+    def regenerate(self):
+        self.maze = [list('#'*width) for _ in range(height)]
+        self._dig_walls()
 
 
 if __name__ == '__main__':
-    maze = GenerateMaze(25, 25)
+
+    def input_check(user):
+        if user < 5 or user % 2 == 0:
+            return False
+        else:
+            return True
+
+    while True:
+        width = int(input('Width = '))
+        height = int(input('Height = '))
+        if all([input_check(width), input_check(height)]):
+            break
+        else:
+            print('幅と高さは5以上の奇数にしてください。')
+
+    maze = GenerateMaze(width, height)
     maze.preset_start_goal()
     maze.print_maze()
